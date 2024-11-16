1f967df7144ffab131175c3fa7a78ac6151b726a
Ravneet kaur <122342037+Revabindra@users.noreply.github.com>
2024-11-16 09:12:57 +0000

    commit changes

--- javascriptfolder/test.js
+++ javascriptfolder/test.js
@@ -1,1 +1,45 @@
 console.log("reva");
+const id= 123
+let name="reva kaur"
+var password="12345"
+email="revabindra"
+var state;
+let country;
+//const city; 
+console.log(id)
+name="archi"
+password="1234567"
+console.table([id,name,password,email,state,country])
+//var not used due to block scope and functional scope issue 
+
+"use strict" // treat all js code as new version 
+//alert(3+3); // on browser only
+
+let naam="efrff"
+let num=234
+let yes=true
+//number 2 to power 53     or bigint
+//string ""
+//boolean true/false
+//null standalone value empty hota
+//undefined 
+//symbol unique
+//object 
+
+console.log(typeof num);
+console.log(typeof naam);
+console.log(typeof null); // object 
+console.log(typeof undefined); //undefined
+
+let score="33abc"
+console.log(typeof score)
+let value=Number(score)
+console.log(typeof value)
+console.log(value)
+let score1=null
+console.log( score1) 
+//"33" = 33
+//"33abc" =Nan
+// true = 1, 0-false, "" false, 
+
+ 
