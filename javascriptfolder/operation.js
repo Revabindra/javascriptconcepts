f2a82899629ce1f11562ea2f9f8f856085b35ef2
Ravneet kaur <122342037+Revabindra@users.noreply.github.com>
2024-11-18 16:42:06 +0000

    commit done

---
+++ javascriptfolder/operation.js
@@ -0,0 +1,45 @@
+// let str1="reva"
+// let str2="kaur"
+// let str3=str1+ str2
+// console.log(str3);
+
+// console.log("1"+2) //12
+// console.log(1+"2") //12
+// console.log("1"+2+3)// 123
+// console.log(1+2+"3") //33
+
+// console.log(null>0)//false
+// console.log(null>=0)//true
+// console.log(null==0)//false
+// > ,< convert null to 0 
+
+// console.log(undefined>0)//false
+// console.log(undefined>=0)//false
+// console.log(undefined==0)//false
+
+
+// console.log("2"==2);   //true
+// console.log("2"===2);  //false
+
+// datatypes how datatypes in memory processed 
+//primitive(call by value) string number boolean null(empty) undefined 
+//symbol(unique value to make)
+// const id=Symbol('abc')
+// const anid=Symbol('abc')
+// console.log(id===anid)      //FALSE
+// bigint
+//non primitive (call by reference) array,object, function
+// TYPE OF NULL =OBJECT 
+
+//JAVASCRIPT DYNAMIC LANGUAGE - no need to declare the data type of variable explicitly  const score=100, const score=false
+
+console.log(typeof Number);  //fun
+console.log(typeof BigInt);//fun
+console.log(typeof null);//obj
+console.log(typeof undefined); //undf
+console.log(typeof boolean);//undef
+console.log(typeof String);//fun
+console.log(typeof Symbol);//fun
+console.log(typeof Object);//fun
+console.log(typeof Array);//fun
+
